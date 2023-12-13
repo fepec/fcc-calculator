f0bdf43b942622b9994dae5d09cdd49ea4a1e97d
fepec <felipe.estrada.prada@gmail.com>
2023-12-13 18:01:22 -0500

    Add behavior on zero. Add clear

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -6,15 +6,32 @@ import Display from './Display'
 
 export default function Calculator() {
     // state
-    const [displayValue, setDisplayValue ] = useState(0)
+    const [displayValue, setDisplayValue] = useState("0")
+    const [firstValue, setFirstValue] = useState(0)
+    const [activeOperation, setActiveOperation] = useState("")
+    const [secondValue, setSecondValue] = useState(0)
 
     // event handler functions
     function handleNumberClick(e) {
-        console.log(Number(e.target.innerHTML))
-    } 
-    
+        let valueArray = displayValue.split('')
+        if (valueArray.length == 1 && valueArray[0] == "0") {
+            valueArray = [e.target.innerHTML]
+        } else {
+            valueArray.push(e.target.innerHTML)
+        }
+        setDisplayValue(valueArray.join(''))
+    }
+
     function handleOperationClick(e) {
+        setFirstValue(Number(displayValue))
         console.log(e.target.innerHTML)
+    }
+
+    function handleClearClick() {
+        setDisplayValue("0");
+        setFirstValue(0)
+        setSecondValue(0)
+        setActiveOperation("")
     }
 
 
@@ -23,32 +40,32 @@ export default function Calculator() {
     for (let i = 0; i < 3; i++) {
         let row = []
         for (let j = 1; j <= 3; j++) {
-            row.push(<NumberButton key={i * 3 + j} k={i * 3 + j} onNumberButtonClick={handleNumberClick}/>)
+            row.push(<NumberButton key={i * 3 + j} k={i * 3 + j} onNumberButtonClick={handleNumberClick} />)
         }
         numpad.push(<div key={"numpadrow" + i} className="num-row d-flex justify-content-end" >
             {row}
         </div>)
     }
     numpad.push(<div key={"numpadrow" + 4} className="num-row d-flex justify-content-end">
-        <OperationButton operation="clear" onOperationButtonClick={handleOperationClick}/>
+        <OperationButton operation="clear" onOperationButtonClick={handleClearClick} />
         <NumberButton key={0} k={0} onNumberButtonClick={handleNumberClick} />
-        <OperationButton operation="decimal" onOperationButtonClick={handleOperationClick}/>
+        <OperationButton operation="decimal" onOperationButtonClick={handleOperationClick} />
     </div>)
 
     return <div className="container d-flex align-items-end justify-content-center">
-        
-            <div className='numpad'>
-            <Display text={displayValue} />
-                {numpad}
-            </div>
-            <div className="operation-pad d-flex flex-column">
-                <OperationButton operation="divide" onOperationButtonClick={handleOperationClick} />
-                <OperationButton operation="multiply" onOperationButtonClick={handleOperationClick}/>
-                <OperationButton operation="subtract" onOperationButtonClick={handleOperationClick}/>
-                <OperationButton operation="add" onOperationButtonClick={handleOperationClick}/>
-                <OperationButton operation="equals" onOperationButtonClick={handleOperationClick}/>
-            </div>
-        </div>
 
-    
+        <div className='numpad'>
+            <Display text={displayValue} />
+            {numpad}
+        </div>
+        <div className="operation-pad d-flex flex-column">
+            <OperationButton operation="divide" onOperationButtonClick={handleOperationClick} />
+            <OperationButton operation="multiply" onOperationButtonClick={handleOperationClick} />
+            <OperationButton operation="subtract" onOperationButtonClick={handleOperationClick} />
+            <OperationButton operation="add" onOperationButtonClick={handleOperationClick} />
+            <OperationButton operation="equals" onOperationButtonClick={handleOperationClick} />
+        </div>
+    </div>
+
+
 }
--- src/components/NumberButton.js
+++ src/components/NumberButton.js
@@ -3,5 +3,5 @@ import React from 'react'
 const numberNames = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
 
 export default function NumberButton({k, onNumberButtonClick}) {
-    return <button key={"num" + k} id={numberNames[k]} className='btn btn-primary m-1' onClick={onNumberButtonClick}>{k} </button>
+    return <button key={"num" + k} id={numberNames[k]} className='btn btn-primary m-1' onClick={onNumberButtonClick}>{k}</button>
 }
